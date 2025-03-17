b40ca3e9bded8cdf932a05d5715e9fd1d0c23178
kooooichi24 <ex.kouichi1224@gmail.com>
2025-03-17 23:48:18 +0900

    chore: fix path format to load and merge resolver files

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // GraphQLスキーマファイルを読み込む
-const resolverFiles = loadFilesSync(join(__dirname, '**/*.resolvers.graphql'));
+const resolverFiles = loadFilesSync(join(__dirname, '**/*.resolvers.ts'));
 
 // リゾルバーを結合
 const resolvers = mergeResolvers(resolverFiles);
