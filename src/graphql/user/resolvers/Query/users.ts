324ac6399119c472ec0eee85b731d9bb2064902d
kooooichi24 <ex.kouichi1224@gmail.com>
2025-03-22 23:08:18 +0900

    feat: Update `users` query resolver

--- src/graphql/user/resolvers/Query/users.ts
+++ src/graphql/user/resolvers/Query/users.ts
@@ -1,5 +1,39 @@
+import { z } from 'zod';
+import { createPaginationCursor, MAX_PAGINATION_FIRST, toConnection } from '../../../utils';
+import type { UserMapper } from '../../schema.mappers';
 import type { QueryResolvers } from './../../../types.generated';
 
-export const users: NonNullable<QueryResolvers['users']> = async (_parent, _arg, ctx) => {
-  return await ctx.prisma.user.findMany();
+const CursorSchema = z.object({
+  name: z.string(),
+  id: z.string(),
+});
+const { decodeCursor, createCursorEncoder } = createPaginationCursor(CursorSchema);
+
+export const users: NonNullable<QueryResolvers['users']> = async (_parent, args, ctx) => {
+  const first = Math.min(args.first, MAX_PAGINATION_FIRST);
+  const decodedCursor = args.after ? decodeCursor(args.after) : undefined;
+
+  const users = await ctx.prisma.$queryRawUnsafe<
+    Array<{
+      id: string;
+      name: string;
+      email: string;
+      created_at: Date;
+      updated_at: Date;
+    }>
+  >(`
+      SELECT * FROM users
+      ${decodedCursor ? `WHERE (name, id) > ('${decodedCursor.name}', '${decodedCursor.id}')` : ''}
+      ORDER BY name ASC, id ASC
+      LIMIT ${first + 1}
+    `);
+
+  return toConnection(
+    users,
+    createCursorEncoder<UserMapper>((user) => ({ id: user.id, name: user.name })),
+    {
+      first: args.first,
+      after: args.after ?? undefined,
+    },
+  );
 };
