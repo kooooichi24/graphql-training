b19bef293133e7dfc9a043b5fc946238b38d0446
kooooichi24 <ex.kouichi1224@gmail.com>
2025-03-22 23:21:56 +0900

    feat: Implement pagination for teams query resolver using cursor-based approach

--- src/graphql/team/resolvers/Query/teams.ts
+++ src/graphql/team/resolvers/Query/teams.ts
@@ -1,5 +1,39 @@
+import { z } from 'zod';
+import { MAX_PAGINATION_FIRST, createPaginationCursor, toConnection } from '../../../utils';
+import type { TeamMapper } from '../../schema.mappers';
 import type { QueryResolvers } from './../../../types.generated';
 
-export const teams: NonNullable<QueryResolvers['teams']> = async (_parent, _arg, ctx) => {
-  return ctx.prisma.team.findMany();
+const CursorSchema = z.object({
+  name: z.string(),
+  id: z.string(),
+});
+const { decodeCursor, createCursorEncoder } = createPaginationCursor(CursorSchema);
+
+export const teams: NonNullable<QueryResolvers['teams']> = async (_parent, args, ctx) => {
+  const first = Math.min(args.first, MAX_PAGINATION_FIRST);
+  const decodedCursor = args.after ? decodeCursor(args.after) : undefined;
+
+  const teams = await ctx.prisma.$queryRawUnsafe<
+    Array<{
+      id: string;
+      name: string;
+      description: string | null;
+      created_at: Date;
+      updated_at: Date;
+    }>
+  >(`
+    SELECT * FROM teams
+    ${decodedCursor ? `WHERE (name, id) > ('${decodedCursor.name}', '${decodedCursor.id}')` : ''}
+    ORDER BY name ASC, id ASC
+    LIMIT ${first + 1}
+  `);
+
+  return toConnection(
+    teams,
+    createCursorEncoder<TeamMapper>((team) => ({ id: team.id, name: team.name })),
+    {
+      first: args.first,
+      after: args.after ?? undefined,
+    },
+  );
 };
