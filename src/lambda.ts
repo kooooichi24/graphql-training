d094d2ddafa2a2d335d492c9bb19450fb7b04e02
kooooichi24 <ex.kouichi1224@gmail.com>
2025-04-06 22:37:15 +0900

    feat: Apply permissions middleware created by Apollo Shield

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,13 +1,21 @@
 import { ApolloServer } from '@apollo/server';
 import { handlers, startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import { type Context, createContext } from './context.js';
 import { resolvers } from './graphql/resolvers.generated.js';
 import { typeDefs } from './graphql/typeDefs.generated.js';
+import { applyMiddleware } from 'graphql-middleware';
+import { permissions } from './middlewares/permissions/index.js';
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+const schemaWithMiddleware = applyMiddleware(schema, permissions);
 
 // Create the Apollo Server with explicit type parameter
 const server = new ApolloServer<Context>({
-  resolvers,
-  typeDefs,
+  schema: schemaWithMiddleware,
 });
 
 // Create the Lambda handler
