d4b354f28fecc5877f20041421fda3096db4d3ae
kooooichi24 <ex.kouichi1224@gmail.com>
2025-04-09 12:36:31 +0900

    style: Format import order

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,10 +1,10 @@
 import { ApolloServer } from '@apollo/server';
 import { handlers, startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { applyMiddleware } from 'graphql-middleware';
 import { type Context, createContext } from './context.js';
 import { resolvers } from './graphql/resolvers.generated.js';
 import { typeDefs } from './graphql/typeDefs.generated.js';
-import { applyMiddleware } from 'graphql-middleware';
 import { permissions } from './middlewares/permissions/index.js';
 
 const schema = makeExecutableSchema({
