5283767fb15ab9425de7b95f69265a407f88fe78
kooooichi24 <ex.kouichi1224@gmail.com>
2025-06-22 16:18:49 +0900

    feat: Add `instrumentation.ts` for OpenTelemetry

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -5,6 +5,7 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { PrismaInstrumentation } from '@prisma/instrumentation';
 
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
@@ -12,14 +13,21 @@ const sdk = new NodeSDK({
     [ATTR_SERVICE_VERSION]: '1.0.0',
   }),
   traceExporter: new OTLPTraceExporter({
-    url: "http://localhost:4318/v1/traces",
+    url: 'http://localhost:4318/v1/traces',
   }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({
-      url: "http://localhost:4318/v1/metrics",
+      url: 'http://localhost:4318/v1/metrics',
     }),
   }),
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-pg': {
+        enabled: false,
+      },
+    }),
+    new PrismaInstrumentation(),
+  ],
 });
 
 sdk.start();
